ea3911bff73acc4d71eb29002d9e5e50c0baf96f
qiubee <45637654+qiubee@users.noreply.github.com>
2022-06-22 16:06:11 +0200

    add Start compontent

---
+++ src/lib/pages/Start.svelte
@@ -0,0 +1,75 @@
+<script></script>
+
+
+<svg xmlns="http://www.w3.org/2000/svg" width="283.449" height="238.5" viewBox="0 0 283.449 238.5" class="sun">
+    <g id="Group_3" data-name="Group 3" transform="translate(-3.275 -23.5)">
+    <g id="Group_1" data-name="Group 1" class="rays">
+        <line id="Line_1" data-name="Line 1" y1="75" transform="translate(146.5 23.5)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_1-2" data-name="Line 1" y1="75" transform="translate(254.978 68.894) rotate(45)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_1-3" data-name="Line 1" y1="75" transform="translate(204.565 35.878) rotate(22.5)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_3" data-name="Line 3" y1="75" transform="translate(72.758 146.346) rotate(112.5)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_1-4" data-name="Line 1" y1="75" transform="translate(88.206 34.73) rotate(-22.5)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_2" data-name="Line 2" y1="75" transform="translate(286.533 117.645) rotate(67.5)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_1-5" data-name="Line 1" y1="55" transform="translate(161.715 99.817) rotate(-168.75)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_1-6" data-name="Line 1" y1="55" transform="translate(131.227 99.231) rotate(168.75)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_2-2" data-name="Line 2" y1="55" transform="translate(221.377 160.318) rotate(-101.25)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_1-7" data-name="Line 1" y1="55" transform="translate(189.942 112.544) rotate(-146.25)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_3-2" data-name="Line 3" y1="55" transform="translate(80.783 132.815) rotate(123.75)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_1-8" data-name="Line 1" y1="55" transform="translate(102.553 110.877) rotate(146.25)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_2-3" data-name="Line 2" y1="55" transform="translate(209.217 132.815) rotate(-123.75)" fill="none" stroke="#9b9188" stroke-width="1"/>
+        <line id="Line_4" data-name="Line 4" y1="75" transform="translate(90.929 120.513) rotate(135)" fill="none" stroke="#9b9188" stroke-width="1"/>
+    </g>
+    <g id="Group_2" data-name="Group 2">
+        <circle id="Ellipse_4" data-name="Ellipse 4" cx="6" cy="6" r="6" transform="translate(73 101)" fill="#fefae2"/>
+        <circle id="Ellipse_5" data-name="Ellipse 5" cx="6" cy="6" r="6" transform="translate(141 44)" fill="#fefae2"/>
+        <circle id="Ellipse_6" data-name="Ellipse 6" cx="6" cy="6" r="6" transform="translate(141 80)" fill="#fefae2"/>
+        <circle id="Ellipse_7" data-name="Ellipse 7" cx="10" cy="10" r="10" transform="translate(75 70)" fill="#fefae2"/>
+        <circle id="Ellipse_8" data-name="Ellipse 8" cx="10" cy="10" r="10" transform="translate(156 72)" fill="#fefae2"/>
+        <circle id="Ellipse_9" data-name="Ellipse 9" cx="10" cy="10" r="10" transform="translate(186 43)" fill="#fefae2"/>
+        <circle id="Ellipse_10" data-name="Ellipse 10" cx="6" cy="6" r="6" transform="translate(174 88)" fill="#fefae2"/>
+        <circle id="Ellipse_11" data-name="Ellipse 11" cx="6" cy="6" r="6" transform="translate(95 60)" fill="#fefae2"/>
+        <circle id="Ellipse_12" data-name="Ellipse 12" cx="6" cy="6" r="6" transform="translate(104 80)" fill="#fefae2"/>
+        <circle id="Ellipse_13" data-name="Ellipse 13" cx="6" cy="6" r="6" transform="translate(220 93)" fill="#fefae2"/>
+        <circle id="Ellipse_14" data-name="Ellipse 14" cx="6" cy="6" r="6" transform="translate(215 120)" fill="#fefae2"/>
+        <circle id="Ellipse_15" data-name="Ellipse 15" cx="6" cy="6" r="6" transform="translate(252 146)" fill="#fefae2"/>
+        <circle id="Ellipse_16" data-name="Ellipse 16" cx="10" cy="10" r="10" transform="translate(40 126)" fill="#fefae2"/>
+    </g>
+    <circle id="Ellipse_1" data-name="Ellipse 1" cx="78" cy="78" r="78" transform="translate(67 106)" fill="#eeb155"/>
+    </g>
+</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width="800" viewBox="0 0 532.048 479.76" class="sand">
+<path id="Path_2" data-name="Path 2" d="M73.091,31.114c50.454-.281,104.6.428,171.737,46.017s107.36,21,205.205,54.985c.841.351-64.569,275.064-64.569,275.064L0,303.895Z" transform="matrix(0.966, -0.259, 0.259, 0.966, -8.045, 86.455)" fill="#e6e4aa"/>
+</svg>  
+<h1>Oase</h1>
+
+<style>
+  h1 {
+    position: relative;
+    font-family: "Kotori Rose";
+    color: white;
+    font-size: 3.5rem;
+    font-weight: 700;
+    margin-top: 16rem;
+    text-align: center;
+    z-index: 1;
+  }
+
+  .sun {
+    position: absolute;
+    transform: scale(3.5) translate(0.25rem, 1rem)
+  }
+
+  .rays {
+    transform: translateY(0.25rem);
+  }
+
+  .rays line {
+    stroke-width: 0.25;
+  }
+
+  .sand {
+    position: absolute;
+    z-index: 1;
+    transform: translate(-18rem, 18rem)
+  }    
+</style>
