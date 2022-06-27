16277938b6145cf854c86c700d661f5baf58e279
qiubee <45637654+qiubee@users.noreply.github.com>
2022-06-27 18:58:33 +0200

    add store vars

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,22 @@
-import { writable } from "svelte/store";
+import { readable, writable } from "svelte/store";
 
-export const loggedIn = writable(false)
-export const onboard = writable(true)
+// test
+export const loggedIn = writable(true);
+export const onboard = writable(true);
+
+// export const loggedIn = writable(false);
+// export const onboard = writable(true);
+export const theme = writable("Oase");
+export const subjects = readable([
+  "studiedruk",
+  "studiebegeleiding",
+  "communicatie",
+  "diversiteit",
+  "studentenvereniging",
+  "informatievoorziening",
+  "studieplekken",
+  "evenementen",
+  "studieadvies",
+  "online",
+]);
+export const interestedSubjects = writable([])
