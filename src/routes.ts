f47c25dbed94a42803819409cf0c72d2e1b8750c
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-04 17:46:22 +0200

    add post route

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import NotFound from "./routes/NotFound.svelte";
 import Notifications from "./routes/Notifications.svelte";
 import Profile from "./routes/Profile.svelte";
 import Search from "./routes/Search.svelte";
+import Post from "./lib/components/Post.svelte";
 import Setup from "./routes/Setup.svelte";
 
 export default {
@@ -15,5 +16,8 @@ export default {
   "/profiel": Profile,
   "/login": Login,
   "/account-instellen": Setup,
+
+  "/post/:id": Post,
+
   "*": NotFound,
 };
