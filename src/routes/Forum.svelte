9a5a33a34be44553b3b8c83b5b06db97daf17ba2
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-12 17:08:38 +0200

    add Forum component

---
+++ src/routes/Forum.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import TopBar from "../lib/components/TopBar.svelte";
+    import NavBar from "../lib/components/NavBar.svelte";
+    import { calcContentView } from "../utils/utils";
+
+    let header: HTMLElement;
+    let navigation: HTMLElement;
+    let contentHeight: number;
+
+    onMount(function () {
+        contentHeight = calcContentView(header, navigation);
+    })
+</script>
+
+<div class="view">
+    <div class="content">
+        <TopBar bind:node={header} state={"forum"}/>
+        <main style="max-height: {contentHeight}px;">
+
+        </main>
+        <NavBar bind:node={navigation} location={"forum"}/>
+    </div>
+</div>
+
+<style>
+
+</style>
