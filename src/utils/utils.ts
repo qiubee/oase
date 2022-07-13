a96d4a43d2fb8887a239a7329f04c8b929e767f1
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-13 18:25:37 +0200

    lint

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -40,7 +40,10 @@ export function shuffle(array: Post[]): Post[] {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
