c535b37a83468bd6e8b16aafe9e46bacaaadfff3
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-13 18:21:23 +0200

    feat: show news, categories rankings & spotlight posts

--- src/routes/Forum.svelte
+++ src/routes/Forum.svelte
@@ -2,11 +2,17 @@
     import { onMount } from "svelte";
     import TopBar from "../lib/components/TopBar.svelte";
     import NavBar from "../lib/components/NavBar.svelte";
-    import { calcContentView } from "../utils/utils";
+    import NewsSlider from "../lib/components/NewsSlider.svelte";
+    import Categories from "../lib/components/Categories.svelte";
+    import Rankings from "../lib/components/Rankings.svelte";
+    import PostCompact from "../lib/components/PostCompact.svelte";
+    import { calcContentView, shuffle } from "../utils/utils";
+    import { posts } from "../store";
 
     let header: HTMLElement;
     let navigation: HTMLElement;
     let contentHeight: number;
+    const spotlightPosts = shuffle($posts).slice(0, 3);
 
     onMount(function () {
         contentHeight = calcContentView(header, navigation);
@@ -17,12 +23,30 @@
     <div class="content">
         <TopBar bind:node={header} state={"forum"}/>
         <main style="max-height: {contentHeight}px;">
-
+            <NewsSlider/>
+            <Categories/>
+            <Rankings/>
+            <div class="spotlight">
+                <h2>Uitgelicht</h2>
+                <ul>
+                {#each spotlightPosts as post (post.id)}
+                    <PostCompact postID={post.id} />
+                {/each}
+                </ul>
+            </div>
         </main>
         <NavBar bind:node={navigation} location={"forum"}/>
     </div>
 </div>
 
 <style>
+    main {
+        overflow-y: auto;
+    }
+
+    h2 {
+        font-size: 1rem;
+        margin: 0 0.75rem;
+    }
 
 </style>
