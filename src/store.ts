3ee00473d1e9e64e6fd7b30f54c4f4695b3778ed
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-13 18:22:21 +0200

    add news var, rename subjects to categories & import corresponding types

--- src/store.ts
+++ src/store.ts
@@ -1,38 +1,22 @@
 import { readable, writable } from "svelte/store";
 import type {
-  Subjects,
   StatusOptions,
   Route,
   Post,
   Theme,
   Student,
   Representative,
+  NewsPost,
+  Category,
 } from "src/@types/main";
+import { allStatusOptions } from "./@types/main";
 import allStudents from "./db/students.json";
 import allRepresentatives from "./db/representatives.json";
 import allPosts from "./db/posts.json";
 import allRoutes from "./db/routes.json";
 import allThemes from "./db/themes.json";
-
-const allSubjects = [
-  "studiedruk",
-  "studiebegeleiding",
-  "communicatie",
-  "diversiteit",
-  "studentenvereniging",
-  "informatievoorziening",
-  "studieplekken",
-  "evenementen",
-  "studieadvies",
-  "online",
-] as const;
-
-const allStatusOptions = [
-  "Online",
-  "Bezig",
-  "Aan het werk",
-  "Niet storen",
-] as const;
+import allNews from "./db/news.json";
+import allCategories from "./db/categories.json";
 
 // test
 export const loggedIn = writable<boolean>(true);
@@ -41,7 +25,7 @@ export const onboard = writable<boolean>(false);
 // export const loggedIn = writable<boolean>(false);
 // export const onboard = writable<boolean>(true);
 export const currentTheme = writable<string>("Oase");
-export const subjects = readable<readonly Subjects[]>(allSubjects);
+export const categories = writable<Category[]>(allCategories);
 export const statusOptions =
   readable<readonly StatusOptions[]>(allStatusOptions);
 export const userID = readable<number>(0);
@@ -51,3 +35,4 @@ export const themes = readable<Theme[]>(allThemes);
 export const students = writable<Student[]>(allStudents);
 export const posts = writable<Post[]>(allPosts);
 export const representatives = writable<Representative[]>(allRepresentatives);
+export const news = readable<NewsPost[]>(allNews);
