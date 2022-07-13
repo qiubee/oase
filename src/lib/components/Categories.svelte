e82434b32f0ad40f4f91ef700a1232755ab0d338
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-13 18:18:53 +0200

    add Categories component

---
+++ src/lib/components/Categories.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { push } from "svelte-spa-router";
+    import { categories } from "../../store";
+
+    export let max: number = $categories.length;
+    const allCategories = $categories;
+</script>
+
+<ul class="categories">
+    {#each allCategories as category, index}
+        {#if index < max}
+            <li on:click={() => push("/categorie/" + category.name)}>{category.name}</li>
+        {/if}
+    {/each}
+</ul>
+
+<style>
+    .categories li:hover {
+        cursor: pointer;
+    }
+
+    .categories {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        margin: 0.75rem 0.75rem;
+    }
+
+    .categories li {
+        display: flex;
+        justify-content: center;
+        width: 7.5rem;
+        background-color: var(--cmd-color-white);
+        padding: 0.6rem 0.75rem;
+        font-family: "Pauschal";
+        font-size: 0.9em;
+        border-radius: 0.25rem;
+        margin-bottom: 0.75rem;
+    }
+</style>
