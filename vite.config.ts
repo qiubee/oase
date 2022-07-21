dae4cae6ac03b91258d2dfc6178c93c6190c7817
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-21 13:21:47 +0200

    remove polyfill script & add browser legacy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
+import legacy from "@vitejs/plugin-legacy";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    legacy({
+      targets: ["defaults", "iOS 9"],
+    }),
+  ],
+  build: {
+    polyfillModulePreload: true,
+  },
+  experimental: {
+    renderBuiltUrl: (filename: string, { hostType } ) => {
+      if (hostType === 'js') {
+        return { runtime: `window.__toCdnUrl(${JSON.stringify(filename)})` }
+      } else {
+        return { relative: true }
+      }
+    }
+  }
 });
