738773f388533e553cbc4443e169fe73deedeecd
qiubee <45637654+qiubee@users.noreply.github.com>
2022-07-22 14:03:24 +0200

    add server & preview ports

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/oase/",
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 5050,
+  },
   plugins: [
     svelte(),
     legacy({
