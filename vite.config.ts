512819384606d7c1533945bc8e4e9e032d6af373
qiubee <45637654+qiubee@users.noreply.github.com>
2022-08-08 13:32:35 +0200

    add postcss plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
+import postcssPreset from "postcss-preset-env";
+import postcssFlexboxFixes from "postcss-flexbugs-fixes";
+import safe from "postcss-safe-parser";
+import postcssCustomProperties from "postcss-custom-properties";
+// import postcss from "rollup-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/oase/",
+  // build: {
+  //   rollupOptions: {
+  //     plugins: [
+  //       postcss({
+  //         extract: "public/build/global.css",
+  //         sourceMap: true,
+  //         minimize: true,
+  //       }),
+  //     ],
+  //   },
+  // },
   server: {
     port: 3000,
   },
@@ -12,7 +29,22 @@ export default defineConfig({
     port: 5050,
   },
   plugins: [
-    svelte(),
+    svelte({
+      preprocess: sveltePreprocess({
+        postcss: {
+          plugins: [
+            safe,
+            postcssPreset({
+              stage: 0,
+              browsers: "iOS 9",
+              autoprefixer: { grid: true },
+            }),
+            postcssFlexboxFixes,
+            postcssCustomProperties,
+          ],
+        },
+      }),
+    }),
     legacy({
       targets: ["defaults", "iOS 9"],
     }),
