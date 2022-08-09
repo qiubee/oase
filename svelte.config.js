a71cae5e7a637a26aadff381f882441e6ffc6d99
qiubee <45637654+qiubee@users.noreply.github.com>
2022-08-09 17:55:41 +0200

    update svelte config & resolve import err

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,24 @@
 import sveltePreprocess from "svelte-preprocess";
+import postcssPreset from "postcss-preset-env";
+import postcssCustomProperties from "postcss-custom-properties";
+import safe from "postcss-safe-parser";
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess(),
+  preprocess: sveltePreprocess({
+    postcss: {
+      parser: safe,
+      plugins: [
+        postcssCustomProperties,
+        postcssPreset({
+          stage: 0,
+          browsers: "> 0.25%, iOS 9",
+          autoprefixer: {
+            grid: true,
+          },
+        }),
+      ],
+    },
+  }),
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,48 @@
+import * as path from "path";
 import { defineConfig } from "vite";
-import legacy from "@vitejs/plugin-legacy";
+// import legacy from "@vitejs/plugin-legacy";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
-import postcssPreset from "postcss-preset-env";
-import postcssFlexboxFixes from "postcss-flexbugs-fixes";
-import safe from "postcss-safe-parser";
-import postcssCustomProperties from "postcss-custom-properties";
+import { babel } from "@rollup/plugin-babel";
 // import postcss from "rollup-plugin-postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/oase/",
-  // build: {
-  //   rollupOptions: {
-  //     plugins: [
-  //       postcss({
-  //         extract: "public/build/global.css",
-  //         sourceMap: true,
-  //         minimize: true,
-  //       }),
-  //     ],
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      plugins: [
+        babel({
+          babelHelpers: "bundled",
+          configFile: path.resolve(__dirname, "babel.config.json"),
+        }),
+        // postcss({
+        //   parser: safe,
+        //   extract: true,
+        //   minimize: true,
+        //   plugins: [
+        //     postcssPreset({
+        //       stage: 0,
+        //       browsers: "iOS 9",
+        //       autoprefixer: { grid: true },
+        //     }),
+        //     postcssFlexboxFixes,
+        //     postcssCustomProperties,
+        //   ],
+        // }),
+      ],
+      // output: {
+      //   plugins: [getBabelOutputPlugin({
+      //     configFile: path.resolve(__dirname, "babel.config.json")
+      //   })
+      //   ]
+      // }
+    },
+  },
   server: {
     port: 3000,
   },
   preview: {
     port: 5050,
   },
-  plugins: [
-    svelte({
-      preprocess: sveltePreprocess({
-        postcss: {
-          plugins: [
-            safe,
-            postcssPreset({
-              stage: 0,
-              browsers: "iOS 9",
-              autoprefixer: { grid: true },
-            }),
-            postcssFlexboxFixes,
-            postcssCustomProperties,
-          ],
-        },
-      }),
-    }),
-    legacy({
-      targets: ["defaults", "iOS 9"],
-    }),
-  ],
+  plugins: [svelte()],
 });
