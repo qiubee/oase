bd35ec84d30edb784050dd9f32ed3086a9339f26
qiubee <45637654+qiubee@users.noreply.github.com>
2022-08-10 20:02:26 +0200

    add smooth scroll polyfill

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 // import "./global.css";
 import App from "./App.svelte";
+import smoothscroll from "smoothscroll-polyfill";
+smoothscroll.polyfill();
 
 const app = new App({
   target: document.getElementById("app"),
